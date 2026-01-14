feb35d07012074ddd11d4851498df648192ff4d3
HarryGuo2 <zguo29@dons.usfca.edu>
2026-01-14 17:53:52 -0500

    Initial Commit

---
+++ AITrainer/AITrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITrainerApp.swift
+//  AITrainer
+//
+//  Created by Harry Guo on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct AITrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
